642926182051e00045ea22a45c5867102c752828
Ice Quezon <icequezon@outlook.com>
2016-05-30 15:54:53 +0800

    Modifed code to make multiple cars move.

--- js/car.js
+++ js/car.js
@@ -3,16 +3,66 @@
 var Car = function(carPath) {
 	var path = carPath;
 	var pathIndex = 1;
-	var dist = Math.sqrt((path[pathIndex+1]['x']-path[pathIndex]['x'])*(path[pathIndex+1]['x']-path[pathIndex]['x'])+
-		(path[pathIndex+1]['y']-path[pathIndex]['y'])*(path[pathIndex+1]['y']-path[pathIndex]['y']));
+	var prevIndex = pathIndex - 1;
+	var dist = 0;
 	this.carCoord = {};
 	this.image = new Image();
-	this.angle = 0;
-	this.carCoord['x'] = 0;
-	this.carCoord['y'] = 0;
 	this.image.src = "resources/cars/taxi.png";
 
+	dist = initDist();
+	this.angle = initAngle();
+	prevIndex = goToEntry();
+	pathIndex = prevIndex + 1;
+	this.carCoord['x'] = carPath[prevIndex]['x'];
+	this.carCoord['y'] = carPath[prevIndex]['y'];
+
 	//Functions
+
+	function goToEntry() {
+		var entryPoints = [];
+		entryPoints.push(0);
+		for (var i = 0; i < path.length; i++) {
+			if(path[i]['type'] == 1) {
+				entryPoints.push(i+1);
+			}
+			if(path[i]['branch'].length > 0 && i < path.length-1 ) {
+				entryPoints.push(i+1);
+			}
+		}
+		
+		var entry = Math.floor((Math.random() * 10)) % entryPoints.length;
+		return entryPoints[entry];
+	}
+
+	function initDist() {
+		//change dist
+		dist = Math.sqrt((path[pathIndex]['x']-path[prevIndex]['x'])*(path[pathIndex]['x']-path[prevIndex]['x'])+
+			(path[pathIndex]['y']-path[prevIndex]['y'])*(path[pathIndex]['y']-path[prevIndex]['y']));
+		return dist;
+	};
+
+	function initAngle(){
+		//change angle
+		var angleY = path[pathIndex]['y'] - path[prevIndex]['y'];
+		var angleX = path[pathIndex]['x'] - path[prevIndex]['x'];
+
+		this.angle = Math.atan2(angleY, angleX) * (180 / Math.PI);
+
+		if (this.angle < 0)
+			this.angle = Math.abs(this.angle);
+		else 
+			this.angle = 360 - this.angle;
+
+		if(angleX > 0) {
+			//if going to the right
+			this.angle = 90 - this.angle;
+		}
+		else {
+			this.angle = 360 + 90 - this.angle;
+		}
+
+		return this.angle;
+	}
 
 	this.move = function() {
 
@@ -20,31 +70,42 @@ var Car = function(carPath) {
 		if(pathIndex < path.length){
 			var x = Math.round(this.carCoord['x']);
 			var y = Math.round(this.carCoord['y']);
-			if(x == path[pathIndex]['x'] || y == path[pathIndex]['y']){
-				pathIndex++;
-				if(pathIndex >= path.length)
-					return;
-				//change angle
-				this.angle = Math.atan2((path[pathIndex]['x'] - path[pathIndex-1]['x']),
-				(path[pathIndex]['y'] - path[pathIndex-1]['y'])) * (180 / Math.PI);
+			if(x == path[pathIndex]['x'] && y == path[pathIndex]['y']){
+				prevIndex = pathIndex;
+				if(path[pathIndex]['branch'].length > 0) {
+					var pathJump = path[pathIndex]['branch'].length;
+					var randomPath = Math.floor((Math.random() * 10) % pathJump);
+					pathIndex = path[pathIndex]['branch'][randomPath];
+				}
+				else
+					pathIndex++;
 
-				if (this.angle < 0)
-					this.angle = Math.abs(this.angle);
-				else 
-				    this.angle = 360 - this.angle;
+				//if type is dead point, go to another point
+				if(path[prevIndex]['type'] == 1 || pathIndex >= path.length){
+					prevIndex = goToEntry();
+					pathIndex = prevIndex + 1;
+					this.carCoord['x'] = path[prevIndex]['x'];
+					console.log(path[prevIndex]['x']);
+					this.carCoord['y'] = path[prevIndex]['y'];
+					dist = initDist();
+					this.angle = initAngle();
+					return;
+				}
+
+				//change angle
+				this.angle = initAngle();
 
 				//change dist
-				dist = Math.sqrt((path[pathIndex]['x']-path[pathIndex-1]['x'])*(path[pathIndex]['x']-path[pathIndex-1]['x'])+
-					(path[pathIndex]['y']-path[pathIndex-1]['y'])*(path[pathIndex]['y']-path[pathIndex-1]['y']));
+				this.dist = initDist();
 			}
 
 			//move Car
-			var velx = (path[pathIndex]['x'] - path[pathIndex-1]['x'])/dist;
-			var vely = (path[pathIndex]['y'] - path[pathIndex-1]['y'])/dist;
-			var x = Math.round(this.carCoord['x']);
-			var y = Math.round(this.carCoord['y']);
-			this.carCoord['x']+=velx;
-			this.carCoord['y']+=vely;
+			var velx = (path[pathIndex]['x'] - path[prevIndex]['x'])/dist;
+			var vely = (path[pathIndex]['y'] - path[prevIndex]['y'])/dist;
+			x = Math.floor(this.carCoord['x']);
+			y = Math.floor(this.carCoord['y']);
+				this.carCoord['x']+=velx;
+				this.carCoord['y']+=vely;
 		}
 	}
 };
@@ -57,12 +118,12 @@ var CarList = function(carPath) {
 	this.drawCars = function(canvas) {
 		for (var i = 0; i < size; i++) {
 			var image = this.cars[i].image;
-			var x = this.cars[i].carCoord['x'];
-			var y = this.cars[i].carCoord['y'];
+			var x = Math.round(this.cars[i].carCoord['x']);
+			var y = Math.round(this.cars[i].carCoord['y']);
 			canvas.save();
-			// canvas.translate(image.width,image.height);
-			// canvas.rotate(Math.PI / 180 * (this.cars[i].angle));
-			canvas.drawImage(image,0,0,image.width,image.height,x,y,50,50);
+			canvas.translate(x,y);
+			canvas.rotate(this.cars[i].angle*(Math.PI / 180));
+			canvas.drawImage(image,0,0,image.width,image.height,-25,-25,50,50);
 			canvas.restore();
 		}
 	}
@@ -76,8 +137,7 @@ var CarList = function(carPath) {
 
 	this.moveCars = function() {
 		for (var i = 0; i < size; i++) {
-		
-				this.cars[i].move();
+			this.cars[i].move();
 		}
 	}
 };
