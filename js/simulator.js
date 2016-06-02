5064476ea273ceb4ca3f83ccda5582c5558f0ebd
Lesterrific17 <mark_lester7@hotmail.com>
2016-06-02 17:12:55 +0800

    improve implementation of recreation of path markers after parsing of data. the path markers are now slightly transparent and under the cars

--- js/simulator.js
+++ js/simulator.js
@@ -149,13 +149,14 @@ function recreatePath(){
 }
 
 function putMarker(x, y, pointType){
-    // if(pointType == 0){
-    //     $('#map-area').append('<div class="pin-pointer go-pt" id="point_' + x + '_' + y + '"></div>');
-    // }
-    // else{
-    //     $('#map-area').append('<div class="pin-pointer dead-pt" id="point_' + x + '_' + y + '"></div>');
-    // } 
-    // $('#point_' + x + '_' + y).css({top: y - 6, left: x - 6});
+     if(pointType == 0){
+         $('#map-area').append('<div class="pin-pointer go-pt" id="point_' + x + '_' + y + '"></div>');
+     }
+     else{
+         $('#map-area').append('<div class="pin-pointer dead-pt" id="point_' + x + '_' + y + '"></div>');
+     } 
+     $('#point_' + x + '_' + y).css({top: y - 6, left: x - 6});
+     $('.pin-pointer').css('opacity', '0.6');
 }
 
 function isvalidJSON(string) {
